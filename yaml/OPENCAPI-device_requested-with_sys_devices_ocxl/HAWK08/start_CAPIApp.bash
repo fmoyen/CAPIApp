12df7b25d7a28570c530435912e812d674dc78a6
Fabrice MOYEN <fabrice_moyen@fr.ibm.com>
2021-11-17 11:45:17 +0100

    start_CAPIapp (Opencapi): added ad9h7 card case
    Signed-off-by: Fabrice MOYEN <fabrice_moyen@fr.ibm.com>

--- yaml/OPENCAPI-device_requested-with_sys_devices_ocxl/HAWK08/start_CAPIApp.bash
+++ yaml/OPENCAPI-device_requested-with_sys_devices_ocxl/HAWK08/start_CAPIApp.bash
@@ -3,11 +3,18 @@
 Node="Hawk08"
 TempFile="/tmp/start_CAPIapp.tmp"
 Choice="nul"
+PvYamlFile=""
+PvcYamlFile=""
+OCYamlFile=""
 
 echo
-echo "List of OpenCAPI cards available on $Node:"
-echo "------------------------------------------"
+echo "List of OpenCAPI cards seen by the Device Plugin on $Node:"
+echo "-----------------------------------------------------------"
 oc describe node hawk08 | sed -n '/Capacity:/,/Allocatable/p' | grep xilinx | grep ocapi | tee $TempFile
+echo
+echo "List of OpenCAPI cards requests / limts on $Node:"
+echo "--------------------------------------------------"
+oc describe node hawk08 | sed -n '/Allocated resources:/,//p' | grep xilinx | grep ocapi
 
 while ! grep -q $Choice $TempFile; do
   echo
@@ -19,9 +26,26 @@ while ! grep -q $Choice $TempFile; do
 done
 
 echo
-echo "your choice is: `grep $Choice $TempFile`"
+echo "your card choice is: `grep $Choice $TempFile`"
+
+
+OCYamlFile=`ls OPENCAPI*${Choice}*.yaml`
+PvYamlFile=`ls sys*${Choice}*pv.yaml`
+PvcYamlFile=`ls sys*${Choice}*pvc.yaml`
 
 echo
 echo "starting the CAPIapp using these yaml files:"
-ls -la *${Choice}*
+echo "--------------------------------------------"
+echo "  PV creation (if needed): $PvYamlFile"
+echo "  PVC creation (if needed): $PvcYamlFile"
+echo "  CAPIapp deployment creation: $OCYamlFile"
+echo
 
+for i in $PvYamlFile $PvcYamlFile $OCYamlFile; do
+  echo 
+  echo "oc create -f $i"
+  oc create -f $i
+  echo 
+done
+
+echo
