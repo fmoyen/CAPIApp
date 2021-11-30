61fdcd97a9ed9712cbdc5412f8b1417689e3fba7
Fabrice MOYEN <fabrice_moyen@fr.ibm.com>
2021-11-30 15:40:40 +0100

    New approach for /sys volume mounts + /lib/modules
    Signed-off-by: Fabrice MOYEN <fabrice_moyen@fr.ibm.com>

--- yaml/start_CAPIApp.bash
+++ yaml/start_CAPIApp.bash
@@ -3,10 +3,14 @@
 Node="hawk08"
 TempFile="/tmp/start_CAPIapp.tmp"
 Choice="nul"
-DevicesPvYamlFile=""
-DevicesPvcYamlFile=""
-BusPvYamlFile=""
-BusPvcYamlFile=""
+OCXL0_DevicesPvYamlFile=""
+OCXL0_DevicesPvcYamlFile=""
+OCXL1_DevicesPvYamlFile=""
+OCXL1_DevicesPvcYamlFile=""
+OCXL0_BusPvYamlFile=""
+OCXL0_BusPvcYamlFile=""
+OCXL1_BusPvYamlFile=""
+OCXL1_BusPvcYamlFile=""
 ImagesDevicePvYamlFile=""
 ImagesDevicePvcYamlFile=""
 OCYamlFile=""
@@ -41,10 +45,17 @@ if `echo $CardChoice | grep -q ocapi`; then
   CardType="Opencapi"
   YamlDir="OPENCAPI-device_requested-with_sys_devices_ocxl/$Node"
   YamlFile=`ls $YamlDir/OPENCAPI-device*${Choice}*deploy.yaml`
-  DevicesPvYamlFile=`ls $YamlDir/sys-devices*${Choice}*pv.yaml`
-  DevicesPvcYamlFile=`ls $YamlDir/sys-devices*${Choice}*pvc.yaml`
-  BusPvYamlFile=`ls $YamlDir/sys-bus*${Choice}*pv.yaml`
-  BusPvcYamlFile=`ls $YamlDir/sys-bus*${Choice}*pvc.yaml`
+
+  OCXL0_DevicesPvYamlFile=`ls $YamlDir/sys-devices-ocxl.0-*${Choice}*pv.yaml`
+  OCXL0_DevicesPvcYamlFile=`ls $YamlDir/sys-devices-ocxl.0-*${Choice}*pvc.yaml`
+  OCXL0_BusPvYamlFile=`ls $YamlDir/sys-bus-ocxl.0-*${Choice}*pv.yaml`
+  OCXL0_BusPvcYamlFile=`ls $YamlDir/sys-bus-ocxl.0-*${Choice}*pvc.yaml`
+
+  OCXL1_DevicesPvYamlFile=`ls $YamlDir/sys-devices-ocxl.1-*${Choice}*pv.yaml`
+  OCXL1_DevicesPvcYamlFile=`ls $YamlDir/sys-devices-ocxl.1-*${Choice}*pvc.yaml`
+  OCXL1_BusPvYamlFile=`ls $YamlDir/sys-bus-ocxl.1-*${Choice}*pv.yaml`
+  OCXL1_BusPvcYamlFile=`ls $YamlDir/sys-bus-ocxl.1-*${Choice}*pvc.yaml`
+
   ImagesDevicePvYamlFile=`ls $YamlDir/images-${Choice}-pv.yaml`
   ImagesDevicePvcYamlFile=`ls $YamlDir/images-${Choice}-pvc.yaml`
 else
@@ -61,39 +72,42 @@ echo " --> $CardType"
 echo
 echo "starting the CAPIapp using these yaml files from $YamlDir directory:"
 echo "------------------------------------------------------------------------------------------------------------------"
-echo "  /sys/devices PV creation (if needed):      `basename $DevicesPvYamlFile 2>/dev/null`"
-echo "  /sys/devices PVC creation (if needed):     `basename $DevicesPvcYamlFile 2>/dev/null`"
-echo "  /sys/bus PV creation (if needed):          `basename $BusPvYamlFile 2>/dev/null`"
-echo "  /sys/bus PVC creation (if needed):         `basename $BusPvcYamlFile 2>/dev/null`"
-echo "  Binary Image PV creation (if needed):      `basename $ImagesDevicePvYamlFile 2>/dev/null`"
-echo "  Binary Image PVC creation (if needed):     `basename $ImagesDevicePvcYamlFile 2>/dev/null`"
+echo "  ocxl.0 /sys/devices PV creation (if needed):      `basename $OCXL0_DevicesPvYamlFile 2>/dev/null`"
+echo "  ocxl.0 /sys/devices PVC creation (if needed):     `basename $OCXL0_DevicesPvcYamlFile 2>/dev/null`"
+echo "  ocxl.1 /sys/devices PV creation (if needed):      `basename $OCXL1_DevicesPvYamlFile 2>/dev/null`"
+echo "  ocxl.1 /sys/devices PVC creation (if needed):     `basename $OCXL1_DevicesPvcYamlFile 2>/dev/null`"
+echo "  ocxl.0 /sys/bus PV creation (if needed):          `basename $OCXL0_BusPvYamlFile 2>/dev/null`"
+echo "  ocxl.0 /sys/bus PVC creation (if needed):         `basename $OCXL0_BusPvcYamlFile 2>/dev/null`"
+echo "  ocxl.1 /sys/bus PV creation (if needed):          `basename $OCXL1_BusPvYamlFile 2>/dev/null`"
+echo "  ocxl.1 /sys/bus PVC creation (if needed):         `basename $OCXL1_BusPvcYamlFile 2>/dev/null`"
+echo "  Binary Image PV creation (if needed):             `basename $ImagesDevicePvYamlFile 2>/dev/null`"
+echo "  Binary Image PVC creation (if needed):            `basename $ImagesDevicePvcYamlFile 2>/dev/null`"
 echo
-echo "  CAPIapp deployment creation:               `basename $YamlFile`"
+echo "  CAPIapp deployment creation:                      `basename $YamlFile`"
 
 echo
-echo "Do you want to choose (for testing purpose) a specific CAPIapp yaml file other than the one indicated above [y|n]?:"
-echo "-------------------------------------------------------------------------------------------------------------------"
-read YamlFileChoice
-if [ "$YamlFileChoice" == "y" ] || [ "$YamlFileChoice" == "Y" ] || [ "$YamlFileChoice" == "yes" ]; then
-  YamlFileOther=""
-  while [ -z "$YamlFileOther" ]; do
-    echo
-    echo "List of available CAPIapp yaml files for $Choice in $YamlDir directory :" 
-    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" 
-    basename -a `ls $YamlDir/*CAPI*$Choice*.yaml`
-    echo
-    echo -e "please give the one that you want: \c"
-    read YamlFileOther
-  done
+YamlFileOther=""
+while [ -z "$YamlFileOther" ]; do
+  echo
+  echo "List of available CAPIapp yaml files for $Choice in $YamlDir directory :" 
+  echo "---------------------------------------------------------------------------------------------------------------------"
+  basename -a `ls $YamlDir/*CAPI*$Choice*.yaml`
+  echo
+  echo "please give the alternative one that you may want, or leave empty if you want to use `basename $YamlFile`:"
+  echo -e "?: \c"
+  read YamlFileOther
+done
+
+if [ "$YamlFileOther" != "" ]; then
   YamlFile=$YamlDir/$YamlFileOther
 else
-  echo "--> so the answer is NO"
+  echo "--> `basename $YamlFile`"
 fi
 
 echo
 echo "starting the CAPIapp:"
 echo "---------------------"
-for i in $DevicesPvYamlFile $DevicesPvcYamlFile $BusPvYamlFile $BusPvcYamlFile $ImagesDevicePvYamlFile $ImagesDevicePvcYamlFile $YamlFile; do
+for i in $OCXL0_DevicesPvYamlFile $OCXL0_DevicesPvcYamlFile $OCXL0_BusPvYamlFile $OCXL0_BusPvcYamlFile $OCXL1_DevicesPvYamlFile $OCXL1_DevicesPvcYamlFile $OCXL1_BusPvYamlFile $OCXL1_BusPvcYamlFile $ImagesDevicePvYamlFile $ImagesDevicePvcYamlFile $YamlFile; do
   echo 
   echo "oc create -f $i"
   oc create -f $i
