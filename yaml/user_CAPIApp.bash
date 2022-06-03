8f185994fe972762e015ecf2d24d76471517b859
Fabrice MOYEN <fabrice_moyen@fr.ibm.com>
2022-06-03 15:40:45 +0200

    user_CAPIApp.bash updated
    Signed-off-by: Fabrice MOYEN <fabrice_moyen@fr.ibm.com>

--- yaml/user_CAPIApp.bash
+++ yaml/user_CAPIApp.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 ################################################################################################################
 # Bash script used when a standard user wants a POD with an OpenCAPI card
 # Author: Fabrice MOYEN (IBM)
@@ -19,53 +21,103 @@
 # VARIABLES
 
 Node="hawk08"
+Menu=1
 UserName=""
 TempFile="/tmp/user_CAPIapp.tmp"
 UserYAMLRootDir=/tmp
 UserResourcesDeleteScript="resourcesDelete.bash"
 CardName="nul"
-OCXL0_Devices_PvYamlFile=""
-OCXL0_Devices_PvcYamlFile=""
-OCXL1_Devices_PvYamlFile=""
-OCXL1_Devices_PvcYamlFile=""
-OCXL0_Bus_PvYamlFile=""
-OCXL0_Bus_PvcYamlFile=""
-OCXL1_Bus_PvYamlFile=""
-OCXL1_Bus_PvcYamlFile=""
-Lib_Modules_PvYamlFile=""
-Lib_Modules_PvcYamlFile=""
-Devices_Pci__Ocxl_PvYamlFile=""
-Devices_Pci_Ocxl__PvcYamlFile=""
-Devices_Pci_PvYamlFile=""
-Devices_Pci_PvcYamlFile=""
 ImagesDevice_PvYamlFile=""
 ImagesDevice_PvcYamlFile=""
-Slots_PhySlot_PvYamlFile=""
-Slots_PhySlot_PvcYamlFile=""
-OCYamlFile=""
 YamlRootDir=""
 SubDir="nul"
 YamlDir=""
 CardType=""
 
 ################################################################################################################
+# FUNCTIONS
+#
+
+function usage
+{
+  echo
+  echo "`basename $0` Usage:"
+  echo "-------------------------"
+  echo
+  echo "Bash script used when a standard user wants a POD with an OpenCAPI card"
+  echo
+  echo "  + No parameters given => `basename $0` starts the menu"
+  echo "  + One parameter given when starting `basename $0` (parameter chosen among the menu possible options) => no menu or question, just doing what has been requested"
+  echo "  + special case: when prune action is chosen as 1st parameter, you may provide nothing as 2nd parameter, or 'all', or any filesystem known by the tool (again see the menu options)"
+  echo
+  echo "  + -h / -? / --help: shows this usage info"
+  echo
+  echo "Example:"
+  echo "--------"
+  echo "`basename $0`"
+  echo "`basename $0` all"
+  echo "`basename $0` list"
+  echo "`basename $0` prune"
+  echo "`basename $0` prune all"
+  echo "`basename $0` prune root"
+  echo
+  exit 0
+}
+
+################################################################################################################
+# CHECKING IF PARAMETERS ARE GIVEN OR WE NEED TO ASK QUESTIONS
+#
+
+if [ $# -gt 0 ]; then
+  Menu=0
+
+  while getopts ":u:c:" option; do
+    case $option in
+      u)
+        UserName=$OPTARG
+      ;;
+      c)
+        CardName=$OPTARG
+      ;;
+      h)
+        usage
+      ;;
+
+      \?) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+      :) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+      *) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+    esac
+  done
+fi
+
+################################################################################################################
 # ASKING FOR THE USER NAME
 
-while [[ "$UserName" == "" ]]; do
+if [ $Menu -eq 1 ]; then
+  while [[ "$UserName" == "" ]]; do
+    echo
+    echo "What is your name ? (no special character) ? :"
+    echo "----------------------------------------------"
+    read UserName
+  done
+
+else
   echo
-  echo "What is your name ? (no special character) ? :"
-  echo "----------------------------------------------"
-  read UserName
-done
+echo "========================================================================================================================================="
+  echo "USER NAME: $UserName"
+echo "========================================================================================================================================="
+fi
 
 ################################################################################################################
 # CHOOSING THE CARD
+
+TrapCmd="rm -f $TempFile"
 
 echo
 echo "List of CAPI/OpenCAPI cards seen by the Device Plugin on $Node:"
 echo "----------------------------------------------------------------"
 oc describe node $Node | sed -n '/Capacity:/,/Allocatable/p' | grep xilinx | tee $TempFile
-trap "rm -f $TempFile" EXIT
+trap "$TrapCmd" EXIT
 
 echo
 echo "List of CAPI/OpenCAPI cards requests / limts on $Node:"
@@ -74,18 +126,23 @@ echo "(0 means no card has been allocated yet)"
 echo -e "\t\t\t\t requests\tlimits"
 oc describe node $Node | sed -n '/Allocated resources:/,//p' | grep xilinx
 
-while ! grep -q $CardName $TempFile; do
-  echo
-  echo "Please choose between the following card:"
-  echo "-----------------------------------------"
-  cat $TempFile | awk -F"-" '{print $2}' | awk -F"_" '{print $1}'
-  echo -e "?: \c"
-  read CardName
-done
+if [ $Menu -eq 1 ]; then
+  while ! grep -q $CardName $TempFile; do
+    echo
+    echo "Please choose between the following card:"
+    echo "-----------------------------------------"
+    cat $TempFile | awk -F"-" '{print $2}' | awk -F"_" '{print $1}'
+    echo -e "?: \c"
+    read CardName
+  done
+fi
 
-echo
 CardFullName=`grep $CardName $TempFile | awk -F":" '{print $1}' | sed 's/ //g'`
-echo "your card choice is: $CardFullName"
+echo
+echo "========================================================================================================================================="
+echo "CARD CHOICE                       : $CardName" 
+echo "FULL REFERENCE OF THE CHOSEN CARD : $CardFullName"
+echo "========================================================================================================================================="
 
 ################################################################################################################
 # OPENCAPI CASE
@@ -115,46 +172,21 @@ else
 fi
 
 ################################################################################################################
-# CHOOSING THE POD YAML DEFINITION FILE
+# CARD TYPE
 
 echo
 echo "Type of card:"
 echo "-------------"
 echo " --> $CardType"
 
-echo
-echo "Starting the CAPIapp using these yaml files from $YamlDir directory:"
-echo "------------------------------------------------------------------------------------------------------------------------"
-echo "  Binary Image PV creation (if needed):              `basename $ImagesDevice_PvYamlFile 2>/dev/null`"
-echo "  Binary Image PVC creation (if needed):             `basename $ImagesDevice_PvcYamlFile 2>/dev/null`"
-echo
-echo "  CAPIapp deployment creation:                      `basename $YamlFile`"
-
-echo
-echo
-echo "List of available CAPIapp yaml files for $CardName in $YamlDir directory :" 
-echo "---------------------------------------------------------------------------------------------------------------------------"
-for i in `ls $YamlDir/*CAPI*.yaml`; do
-   basename -a $i
-done
-
-echo
-echo "please give the alternative one that you may want, or leave empty if you want to use `basename $YamlFile`:"
-echo -e "?: \c"
-read YamlFileOther
-
-if [ "$YamlFileOther" != "" ]; then
-  YamlFile=$YamlDir/$YamlFileOther
-else
-  echo "--> `basename $YamlFile`"
-fi
-
 ################################################################################################################
 # BUILDING THE YAML FILES (IMAGES+POD) WITH THE GIVEN INFO
 
 UserYAMLDir="$UserYAMLRootDir/$UserName"
+#TrapCmd="$TrapCmd; rm -rf $UserYAMLDir"
+
 mkdir -p $UserYAMLDir
-#trap "rm -f $TempFile; rm -rf $UserYAMLDir" EXIT
+#trap "$TrapCmd" EXIT
 
 #---------------------------------------------------------------------------------------------------------------
 # Replacing <USER> / <CARD> by $UserName / $CardName in the yaml definition files
@@ -165,6 +197,8 @@ echo "----------------------------------------------------------------"
 echo "  + <USER> --> $UserName"
 echo "  + <CARD> --> $CardName"
 echo "  + <CARD_REF> -->$CardFullName"
+echo
+echo "(from $YamlDir yaml files)"
 
 for file in $ImagesDevice_PvYamlFile $ImagesDevice_PvcYamlFile $YamlFile; do
   sed "s!<USER>!$UserName!g; s!<CARD>!$CardName!g; s!<CARD_REF>!$CardFullName!g" $file > $UserYAMLDir/`basename $file`
@@ -177,27 +211,57 @@ UserYamlFile="$UserYAMLDir/`basename $YamlFile`"
 #---------------------------------------------------------------------------------------------------------------
 # Building the script that will be responsible for deleting all user resources (PV, PVC, POD, etc)
 
-cat <<EOF >> $UserYAMLDir/$UserResourcesDeleteScript
+cat <<EOF > $UserYAMLDir/$UserResourcesDeleteScript
 #!/bin/bash
 
 # Script that will delete all resources for the user $UserName (PV, PVC, POD, etc)
 
+echo
+echo "========================================================================================================================================="
+echo "DELETING RESOURCES FOR USER $UserName..."
+echo
+echo "Warning: PVC deletion may take a minute as it needs to wait for Pod complete deletion"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------"
+
 # Deleting the Deployment (with the ReplicatSet and the Pod coming with it)
+echo
+echo "oc -n fabriceproject delete deployment.apps/oc-$UserName-$CardName"
 oc -n fabriceproject delete deployment.apps/oc-$UserName-$CardName
 
 # Deleting PVC & PV
+echo
+echo "oc -n fabriceproject delete persistentvolumeclaim/images-$UserName-pvc"
 oc -n fabriceproject delete persistentvolumeclaim/images-$UserName-pvc
+
+echo
+echo "oc delete persistentvolume/images-$UserName"
 oc delete persistentvolume/images-$UserName
+
+# Deleting the user Yaml directory
+echo
+echo "Deleting $UserYAMLDir directory"
+echo "rm -rf $UserYAMLDir"
+rm -rf $UserYAMLDir
+
+echo "========================================================================================================================================="
+echo
 EOF
 
 chmod u+x $UserYAMLDir/$UserResourcesDeleteScript
 
 ################################################################################################################
-# STARTING THE POD
+# DISPLAYING THE USER YAML DEFINITION FILES AND STARTING THE POD
 
 echo
-echo "starting the CAPIapp:"
-echo "---------------------"
+echo "========================================================================================================================================="
+echo "STARTING THE POD USING THESE DEFINITION YAML FILES FROM $UserYAMLDir DIRECTORY:"
+echo "------------------------------------------------------------------------------------------------------------------------"
+echo "  Binary Image PV creation :                 `basename $UserImagesDevice_PvYamlFile`"
+echo "  Binary Image PVC creation :                `basename $UserImagesDevice_PvcYamlFile`"
+echo
+echo "  CAPIapp deployment creation:               `basename $UserYamlFile`"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------"
+
 for i in $UserImagesDevice_PvYamlFile $UserImagesDevice_PvcYamlFile $UserYamlFile; do
   echo 
   echo "oc create -f $i"
@@ -208,8 +272,18 @@ for i in $UserImagesDevice_PvYamlFile $UserImagesDevice_PvcYamlFile $UserYamlFil
       cat $TempFile
     fi
   fi
-
-  echo 
 done
 
+echo "========================================================================================================================================="
+echo
+
+################################################################################################################
+# DISPLAYING THE BASH SCRIPT GENERATED FOR DELETING THE USER RESOURCES
+
+echo
+echo "========================================================================================================================================="
+echo "SCRIPT TO USE IN ORDER TO DELETE THE USER RESOURCES (PV, PVC, POD, etc):"
+echo "------------------------------------------------------------------------"
+echo "  Bash script to delete the user resources:      $UserYAMLDir/$UserResourcesDeleteScript"
+echo "========================================================================================================================================="
 echo
