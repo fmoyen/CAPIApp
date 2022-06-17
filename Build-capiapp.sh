d787a4d350d3b3ff74aaa60f6144156e34710238
Fabrice MOYEN <fabrice_moyen@fr.ibm.com>
2022-06-17 09:58:25 +0200

    Build-capiapp: added -n (--no-cache) and -h options
    Signed-off-by: Fabrice MOYEN <fabrice_moyen@fr.ibm.com>

--- Build-capiapp.sh
+++ Build-capiapp.sh
@@ -1,8 +1,79 @@
 #!/bin/bash
 
+################################################################################################################
+# Script used for generating the docker CAPIApp Image and push it to the docker hub
+# Author: Fabrice MOYEN (IBM)
+
+
+################################################################################################################
+# VARIABLES
+
 docker_repository="fmoyen/capiapp"
 ScriptDir=`realpath $0`
 ScriptDir=`dirname $ScriptDir`
+NoCache=""
+
+
+################################################################################################################
+# FUNCTIONS
+
+function usage
+{
+  echo
+  echo "===================================================================================================="
+  echo "`basename $0` Usage:"
+  echo "-------------------------"
+  echo
+  echo "Bash script used for generating the docker CAPIApp image and push it to the docker hub"
+  echo
+  echo "  + The parameters below are optional"
+  echo
+  echo "  + -n :  uses the --no-cache docker build option, forcing docker to re-build everything"
+  echo "          (by default the cache is used)"
+  echo
+  echo "  + -h : shows this usage info"
+  echo
+  echo "Example:"
+  echo "--------"
+  echo "`basename $0`"
+  echo "`basename $0` -n"
+  echo
+  echo "===================================================================================================="
+  echo "For generating the CAPPapp application, this script $0 will use following steps:"
+  echo
+  echo " - Run docker command docker build -t [docker-repository]:[docker-tag] . to create a docker image of the new capiapp"
+  echo " - Run docker command docker tag -t [docker-repository]:[docker-tag] [docker-repository]:latest to create a docker image of the new capiapp with latest tag"
+  echo " - Check the new generating daemonset docker image with command docker images"
+  echo " - Push the new docker image to a public dockerhub repository"
+  echo
+  echo "This script will use the following dockerhub repository: $docker_repository"
+  echo "===================================================================================================="
+  echo
+  exit 0
+}
+
+
+################################################################################################################
+# CHECKING IF PARAMETERS ARE GIVEN
+
+while getopts ":nh" option; do
+  case $option in
+    n)
+      NoCache="--no-cache"
+    ;;
+    h)
+      usage
+    ;;
+
+    \?) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+    :) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+  esac
+done
+
+
+################################################################################################################
+# MAIN
 
 echo; echo "===================================================================================================="
 echo "===================================================================================================="
@@ -43,7 +114,7 @@ fi
 echo; echo "========================================================"
 echo "Generating the docker image ${docker_repository}:$docker_tag"
 cd $ScriptDir
-docker build -t ${docker_repository}:$docker_tag .
+docker build $NoCache -t ${docker_repository}:$docker_tag .
 
 echo; echo "========================================================"
 echo "Tagging with \"latest\" the new docker image"
